762c8c66b9cc0ef38f46f77a91a83fecdf308fb3
maulidul <rez.maulidul@gmail.com>
2017-01-04 20:15:35 +0700

    s

--- application/views/login.php
+++ application/views/login.php
@@ -10,26 +10,26 @@
          <LINK REL="SHORTCUT ICON" HREF="<?php  echo base_url();?>assets/img/download.png">
         <link href="<?php echo base_url(); ?>assets/css/chosen.css" rel='stylesheet' tyle="text/css">
         <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <link href="<?php echo base_url(); ?>assets/css/theme/avocado.css" rel="stylesheet" type="text/css" id="theme-style">
-        <link href="<?php echo base_url(); ?>assets/css/prism.css" rel="stylesheet/less" type="text/css">
-        <link href='<?php echo base_url(); ?>assets/css/fullcalendar.css' rel='stylesheet' tyle="text/css">
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,400,600,300' rel='stylesheet' type='text/css'> 
+        <!-- <link href="<?php echo base_url(); ?>assets/css/theme/avocado.css" rel="stylesheet" type="text/css" id="theme-style"> -->
+        <!-- <link href="<?php echo base_url(); ?>assets/css/prism.css" rel="stylesheet/less" type="text/css"> -->
+        <!-- <link href='<?php echo base_url(); ?>assets/css/fullcalendar.css' rel='stylesheet' tyle="text/css"> -->
+        <link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,400,600,300' rel='stylesheet' type='text/css'> 
         <style type="text/css">
             body { padding-top: 102px; }
         </style>
         <link href="<?php echo base_url(); ?>assets/css/bootstrap-responsive.css" rel="stylesheet">
 
         <!-- JavaScript/jQuery, Pre-DOM -->
-        <script src="<?php echo base_url(); ?>assets/js/jquery-ui-1.10.0/jquery-1.9.0.js"></script>
+        <!-- <script src="<?php echo base_url(); ?>assets/js/jquery-ui-1.10.0/jquery-1.9.0.js"></script>
         <script src="<?php echo base_url(); ?>assets/js/jquery-ui-1.10.0/ui/jquery.ui.core.js"></script>
         <script src="<?php echo base_url(); ?>assets/js/jquery-ui-1.10.0/ui/jquery.ui.widget.js"></script>
         <script src="<?php echo base_url(); ?>assets/js/jquery-ui-1.10.0/ui/jquery.ui.datepicker.js"></script>
-        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/js/jquery-ui-1.10.0/themes/base/jquery.ui.all.css">
-        <script src="<?php echo base_url(); ?>assets/js/charts/excanvas.min.js"></script>
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/js/jquery-ui-1.10.0/themes/base/jquery.ui.all.css"> -->
+        <!-- <script src="<?php echo base_url(); ?>assets/js/charts/excanvas.min.js"></script>
         <script src="<?php echo base_url(); ?>assets/js/charts/jquery.flot.js"></script>
         <script src="<?php echo base_url(); ?>assets/js/jquery.jpanelmenu.min.js"></script>
         <script src="<?php echo base_url(); ?>assets/js/jquery.cookie.js"></script>
-        <script src="<?php echo base_url(); ?>assets/js/avocado-custom-predom.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/avocado-custom-predom.js"></script> -->
         <script>
             $(function() {
                 $( "#a" ).datepicker();
@@ -155,7 +155,7 @@
     <!-- Javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src='assets/js/jquery.hotkeys.js'></script>
+    <!-- <script src='assets/js/jquery.hotkeys.js'></script>
     <script src='assets/js/calendar/fullcalendar.min.js'></script>
     <script src="assets/js/jquery-ui-1.10.2.custom.min.js"></script>
     <script src="assets/js/jquery.pajinate.js"></script>
@@ -169,17 +169,7 @@
     <script src="assets/js/bootstrap/bootstrap-typeahead.js"></script>
     <script src="assets/js/jquery.easing.min.js"></script>
     <script src="assets/js/jquery.chosen.min.js"></script>
-    <script src="assets/js/avocado-custom.js"></script>
+    <script src="assets/js/avocado-custom.js"></script> -->
 
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','../../../www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-43253177-1', 'lycheedesigns.com');
-        ga('send', 'pageview');
-
-    </script>	
 
 </html>
